80a2d910a7d88ab4a5ce57764ca1cf5ef1af6fad
Neil  Sayers <neil.sayers17@imperial.ac.uk>
2019-05-25 15:19:55 +0100

    [ns1017] working on saving entries

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'list.dart';
+import 'entry.dart';
+import 'scopebase.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Confidant());
 
-class MyApp extends StatelessWidget {
+class Confidant extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ScopeBaseWidget(
+        child: MaterialApp(
       title: 'Confidant',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
@@ -17,80 +20,117 @@ class MyApp extends StatelessWidget {
           body1: TextStyle(fontSize: 18),
         ),
       ),
-      home: NotePage(title: 'Confidant'),
-    );
+      home: ListPage(),
+      initialRoute: '/entry',
+      routes: {
+        '/entry': (context) => EntryPage(new Entry()),
+        '/list': (context) => ListPage()
+      },
+    ));
   }
 }
 
-class NotePage extends StatefulWidget {
-  NotePage({Key key, this.title}) : super(key: key);
-  final String title;
+class EntryPage extends StatefulWidget {
+  EntryPage(this.entry);
+
+  final Entry entry;
 
   @override
-  _NotePageState createState() => _NotePageState();
+  _EntryPageState createState() => _EntryPageState(entry);
 }
 
-class _NotePageState extends State<NotePage> {
+class _EntryPageState extends State<EntryPage> {
   static const num NUM_CHARS_IN_DATE = 10;
   String nowString =
-  new DateTime.now().toIso8601String().substring(0, NUM_CHARS_IN_DATE);
-  TextEditingController _titleController;
+      new DateTime.now().toIso8601String().substring(0, NUM_CHARS_IN_DATE);
+  Entry entry;
+  String initialBody;
+  String initialTitle;
 
-  _NotePageState() {
-    _titleController = new TextEditingController(text: nowString);
+  _EntryPageState(this.entry) {
+    initialBody = entry.body;
+    initialTitle = entry.title;
   }
+
+  // Create a global key that will uniquely identify the Form widget and allow
+  // us to validate the form
+  var _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ListPage()));
-                },
-              ),
-              Expanded(
-                child: TextFormField(
-                  controller: _titleController,
-                  textCapitalization: TextCapitalization.sentences,
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .title,
-                  decoration: InputDecoration(border: InputBorder.none),
+    return WillPopScope(
+        onWillPop: () async {
+          ScopeBaseWidget.of(context).bloc.refresh();
+          return true;
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: initialTitle,
+                      onSaved: (s) => entry.title = s,
+                      validator: (s) => s.length > 2 ? null : 'Give it a title',
+                      // i do not know what this is
+                      textCapitalization: TextCapitalization.sentences,
+                      style: Theme.of(context).textTheme.title,
+                      decoration: InputDecoration(border: InputBorder.none),
+                    ),
+                  )
+                ]),
+          ),
+          body: Container(
+            padding: EdgeInsets.all(15),
+            height: double.infinity,
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      initialValue: initialBody,
+                      onSaved: (s) => entry.body = s,
+                      validator: (s) => s.length > 2 ? null : 'Give it a body',
+                      textCapitalization: TextCapitalization.sentences,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      style: Theme.of(context).textTheme.body1,
+                      decoration:
+                          InputDecoration.collapsed(hintText: 'Write Note'),
+                    ),
+                  ],
                 ),
-              )
-            ]),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(15),
-        height: double.infinity,
-        child: SingleChildScrollView(
-          child: Form(
-            child: Column(
+              ),
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            color: Theme.of(context).buttonColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                TextFormField(
-                  //validator: (s) => s.length > 2 ? null : 'Write Some Note', // i do not know what this is
-                  textCapitalization: TextCapitalization.sentences,
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .body1,
-                  decoration: InputDecoration.collapsed(hintText: 'Write Note'),
+                FlatButton(
+                  onPressed: () {
+                    // this calls 'validate' on all widgets in current state
+                    if (_formKey.currentState.validate()) {
+                      // calls 'save' on all widgets in current state
+                      _formKey.currentState.save();
+                      entry.save();
+                    }
+                  }, //Save Button
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.save,
+                          color: Theme.of(context).iconTheme.color),
+                      SizedBox(width: 3),
+                      Text('Save')
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
