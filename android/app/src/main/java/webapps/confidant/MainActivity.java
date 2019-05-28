06a3b5de724fcdf25c4403d8709f5bd4616db9f8
mr3317 <mr3317@ic.ac.uk>
2019-05-28 18:57:03 +0100

    added Android method for API requests

--- android/app/src/main/java/webapps/confidant/MainActivity.java
+++ android/app/src/main/java/webapps/confidant/MainActivity.java
@@ -10,4 +10,22 @@ public class MainActivity extends FlutterActivity {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
   }
+
+
+  // Makes an API call providing emotional analysis on given text object
+  private String makeAnalysisCall(Object obj) {
+    String textToAnalyze = obj.toString();
+    IamOptions options = new IamOptions.Builder()
+            .apiKey("X0CnXwe429eh-ZgHkiduRutrd3DjP3x0kFH9qVovE4ze").build();
+
+    ToneAnalyzer toneAnalyzer = new ToneAnalyzer("2019-05-23",options);
+    toneAnalyzer
+            .setEndPoint("https://gateway-lon.watsonplatform.net/tone-analyzer/api");
+
+    ToneOptions toneOptions = new ToneOptions.Builder().text(textToAnalyze).build();
+    ToneAnalysis toneAnalysis = toneAnalyzer.tone(toneOptions).execute().getResult();
+    return toneAnalysis.getDocumentTone().toString();
+
+  }
+
 }
