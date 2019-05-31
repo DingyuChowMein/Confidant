99179945d7eea226829c55e02ffff81a577d6efa
ns1017 <ns1017@ic.ac.uk>
2019-05-31 13:08:07 +0100

    [ns1017] added three widget tests, for the entrypage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:confidant/page/list.dart';
+import 'package:confidant/page/entrypage.dart';
+import 'package:confidant/widget/entrytextinput.dart';
 
 import 'package:confidant/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    /*// Build our app and trigger a frame.
+  /*// Build our app and trigger a frame.
     await tester.pumpWidget(Confidant());
 
     // Verify that our counter starts at 0.
@@ -26,5 +28,48 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);*/
+
+  testWidgets('new entry page starts with "Untitled"',
+      (WidgetTester tester) async {
+    Widget newEntryPageWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(home: EntryPage.newEntry()));
+
+    await tester.pumpWidget(newEntryPageWidget);
+
+    expect(find.text('Untitled'), findsOneWidget);
+  });
+
+  testWidgets('new entry page starts with empty body',
+      (WidgetTester tester) async {
+    Widget newEntryPageWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(home: EntryPage.newEntry()));
+
+    await tester.pumpWidget(newEntryPageWidget);
+
+    expect(find.text(''), findsOneWidget);
+  });
+
+  testWidgets(
+      'entrytextinput takes available vertical space, and all horizontal space',
+      (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding =
+        TestWidgetsFlutterBinding.ensureInitialized();
+
+    await binding.setSurfaceSize(Size(1280, 720));
+
+    Widget newEntryPageWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(home: EntryPage.newEntry()));
+
+    await tester.pumpWidget(newEntryPageWidget);
+
+    Size appbarSize = tester.getSize(find.byType(AppBar));
+    Size textEntrySize = tester.getSize(find.byType(EntryTextInput));
+    Size bottomBarSize = tester.getSize(find.byType(BottomAppBar));
+    expect(
+        appbarSize.height + textEntrySize.height + bottomBarSize.height, 720);
+    expect(textEntrySize.width, 1280);
   });
 }
